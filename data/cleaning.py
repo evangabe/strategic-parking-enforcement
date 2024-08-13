90bc5c3fafef1e34526433e366478f46e421d25b
evangabe <ejgabrie@usc.edu>
2024-08-13 16:41:37 -0700

    documentation and cleaning

--- data/cleaning.py
+++ data/cleaning.py
@@ -1,5 +1,91 @@
+import json
 import numpy as np
 import pandas as pd
+import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+VERSION = 0
+DEFAULT_FILE_PATH = f"data/pickle/violations_v{VERSION}.pickle"
+LACITY_ENDPOINT_URL = "https://data.lacity.org/resource/4f5p-udkv.json"
+NUM_THREADS = 10
+
+def fetch_data(limit, offset):
+    response = requests.get(f'{LACITY_ENDPOINT_URL}?$limit={limit}&$offset={offset}')
+    return json.loads(response.text)
+
+def process_chunk(chunk):
+    df = pd.DataFrame(chunk)
+    return transform_dataset(df)
+
+def pickle_dataset(output_path=DEFAULT_FILE_PATH):
+    violations_df_list = []
+    limit = 50000
+    total_rows = 0
+    chunk_size = 50000
+    more_data = True    
+
+    with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+        futures = []
+        
+        while more_data:
+            current_rows = total_rows
+            print(f'Rows extracted: {current_rows}')
+            
+            # Submit a new fetch job
+            futures.append(executor.submit(fetch_data, limit, current_rows))
+            
+            # Process the completed futures
+            if len(futures) >= NUM_THREADS:
+                completed_futures = []
+                for future in as_completed(futures):
+                    data_chunk = future.result()
+                    if data_chunk:  # Check if data_chunk is not empty
+                        processed_chunk = process_chunk(data_chunk)
+                        violations_df_list.append(processed_chunk)
+                        total_rows += len(processed_chunk)
+                        if len(data_chunk) < limit or total_rows > 21600000:
+                            print("No more data available. Exiting loop.")
+                            more_data = False
+                            break
+                    completed_futures.append(future)
+
+                # Remove completed futures from the list
+                futures = [f for f in futures if f not in completed_futures]
+
+        print("Finished primary loop. Processing remaining futures . . .")
+
+        # Process any remaining futures
+        for future in as_completed(futures):
+            data_chunk = future.result()
+            if data_chunk:  # Check if data_chunk is not empty
+                processed_chunk = process_chunk(data_chunk)
+                violations_df_list.append(processed_chunk)
+                total_rows += len(processed_chunk)
+    
+
+    print("Processing complete! Concatenating all data chunks . . .")
+
+    # Concatenate all chunks into a single DataFrame
+    final_df = pd.concat(violations_df_list, ignore_index=True)
+
+    shape = final_df.shape
+    print(f"\n\nDataFrame created with {shape[0]} rows and {shape[1]} columns.")
+
+    final_df.dropna(subset=['issue_date', 'issue_time', 'violation_code'], inplace=True)
+
+    print(f"{shape[0] - final_df.shape[0]} rows with missing data were dropped. \nPickling and storing data at {output_path} . . .")
+    
+    # Store as .pickle file
+    final_df.to_pickle(output_path)
+
+def transform_dataset(df):
+    return df[['issue_date', 'issue_time', 'fine_amount', 'agency', 'violation_code', 'loc_lat', 'loc_long']]
+
+def get_dataset_from_pickle(input_path=DEFAULT_FILE_PATH):
+    """
+    
+    """
+    return pd.read_pickle(input_path)
 
 getTime = lambda x: f"{x//100:02}:{x%100:02}:{np.random.randint(0, 59):02}.{np.random.randint(0, 999):03}"
 
@@ -10,3 +96,4 @@ def merge_datetime(df, date_col='issue_date', time_col='issue_time'):
 
     # Return combined datetime column
     return pd.to_datetime(df['issue_date'] + ' ' + df['issue_time'])
+
--- utils.py
+++ utils.py
@@ -1,88 +1,31 @@
-import json
-import pandas as pd
-import requests
-from concurrent.futures import ThreadPoolExecutor, as_completed
+import os
+from dotenv import load_dotenv
 
-VERSION = 0
-DEFAULT_FILE_PATH = f"data/pickle/violations_v{VERSION}.pickle"
-LACITY_ENDPOINT_URL = "https://data.lacity.org/resource/4f5p-udkv.json"
-NUM_THREADS = 10
+load_dotenv()
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
+# import plotly.express as px
+# import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+plt.style.use("seaborn-v0_8-notebook")
+import seaborn as sns
 
-def fetch_data(limit, offset):
-    response = requests.get(f'{LACITY_ENDPOINT_URL}?$limit={limit}&$offset={offset}')
-    return json.loads(response.text)
+# px.set_mapbox_access_token(os.getenv('MAPBOX_TOKEN'))
 
-def process_chunk(chunk):
-    df = pd.DataFrame(chunk)
-    return transform_dataset(df)
 
-def pickle_dataset(output_path=DEFAULT_FILE_PATH):
-    violations_df_list = []
-    limit = 50000
-    total_rows = 0
-    chunk_size = 50000
-    more_data = True    
+def adfuller_test(ts):
+    result = adfuller(ts)
+    labels = ['ADF Test Statistic', 'p-value', '#Lags Used', 'Number of Observations Used']
+    for val, label in zip(result, labels):
+        print(f"{label}: {val}")
+    if result[1] <= 0.05:
+        print("Data is stationary => timeseries rejects ADF null hypothesis")
+    else:
+        print("Data is non-stationary => timeseries does not reject null hypothesis")
 
-    with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
-        futures = []
-        
-        while more_data:
-            current_rows = total_rows
-            print(f'Rows extracted: {current_rows}')
-            
-            # Submit a new fetch job
-            futures.append(executor.submit(fetch_data, limit, current_rows))
-            
-            # Process the completed futures
-            if len(futures) >= NUM_THREADS:
-                completed_futures = []
-                for future in as_completed(futures):
-                    data_chunk = future.result()
-                    if data_chunk:  # Check if data_chunk is not empty
-                        processed_chunk = process_chunk(data_chunk)
-                        violations_df_list.append(processed_chunk)
-                        total_rows += len(processed_chunk)
-                        if len(data_chunk) < limit or total_rows > 21600000:
-                            print("No more data available. Exiting loop.")
-                            more_data = False
-                            break
-                    completed_futures.append(future)
-
-                # Remove completed futures from the list
-                futures = [f for f in futures if f not in completed_futures]
-
-        print("Finished primary loop. Processing remaining futures . . .")
-
-        # Process any remaining futures
-        for future in as_completed(futures):
-            data_chunk = future.result()
-            if data_chunk:  # Check if data_chunk is not empty
-                processed_chunk = process_chunk(data_chunk)
-                violations_df_list.append(processed_chunk)
-                total_rows += len(processed_chunk)
-    
-
-    print("Processing complete! Concatenating all data chunks . . .")
-
-    # Concatenate all chunks into a single DataFrame
-    final_df = pd.concat(violations_df_list, ignore_index=True)
-
-    shape = final_df.shape
-    print(f"\n\nDataFrame created with {shape[0]} rows and {shape[1]} columns.")
-
-    final_df.dropna(subset=['issue_date', 'issue_time', 'violation_code'], inplace=True)
-
-    print(f"{shape[0] - final_df.shape[0]} rows with missing data were dropped. \nPickling and storing data at {output_path} . . .")
-    
-    # Store as .pickle file
-    final_df.to_pickle(output_path)
-
-def transform_dataset(df):
-    return df[['issue_date', 'issue_time', 'fine_amount', 'agency', 'violation_code', 'loc_lat', 'loc_long']]
-
-def get_dataset_from_pickle(input_path=DEFAULT_FILE_PATH):
-    """
-    
-    """
-    return pd.read_pickle(input_path)
-
+def create_weekly_heatmap(ts, values_col='citation_count'):
+    plt.figure(figsize=(12,12))
+    sns.heatmap(ts.groupby([ts.index.hour,ts.index.weekday])[values_col].mean().unstack(), xticklabels = ['Mon','Tues','Wed','Thurs','Fri','Sat','Sun'], linewidths=1)
+    plt.title('[Hollywood] Average Citation Count',fontdict={'fontsize':22})
+    plt.ylabel('Hour',fontdict={'fontsize':16})
+    plt.xlabel('Day of Week',fontdict={'fontsize':16})
